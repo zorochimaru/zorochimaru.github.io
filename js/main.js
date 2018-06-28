7b8dad96135b5d057cd97bbaa8ad439226a92ad7
zorochimaru <rasim.karimli@gmail.com>
2018-06-28 12:14:54 +0400

    preloader fix

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-$(window).on('load', function() { // makes sure the whole site is loaded 
-    $('#status').fadeOut(); // will first fade out the loading animation 
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-    $('body').delay(350).css({ 'overflow': 'visible' });
-
-
-});
-
 $(document).ready(function() {
     $('#fullpage').fullpage({
        	// lockAnchors: true,
