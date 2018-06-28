7e82f55e876d7e615c2f57990b8390fef0963f29
zorochimaru <rasim.karimli@gmail.com>
2018-06-28 10:52:12 +0400

    upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# zorochimaru.github.io Rasim Karimli CV
---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(350).css({ 'overflow': 'visible' });
+
+
+});
+
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+       	// lockAnchors: true,
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        menu: '#myMenu',
+        verticalCentered: true,
+    });
+
+
+    new WOW().init();
+
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+});
